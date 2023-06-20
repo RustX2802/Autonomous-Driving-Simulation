c2ab08ac5be31afa84bcd0cda41dbd88ec1835b4
RustX <rustix260685@gmail.com>
2023-06-20 17:43:52 +0900

    road

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ myCanvas.height=window.innerHeight;
 myCanvas.width=200;
 
 const ctx=myCanvas.getContext("2d");
+const road=new Road(myCanvas.width/2,myCanvas.width*0.9);
 const car=new Car(100,100,30,50);
 
 animate();
@@ -9,6 +10,7 @@ animate();
 function animate(){
     myCanvas.height=window.innerHeight;
     car.update();
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
