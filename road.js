5b10ce0b8a364956397d951f0f90fdaebbb14297
RustX <rustix260685@gmail.com>
2023-06-21 17:20:22 +0900

    curved road

--- road.js
+++ road.js
@@ -3,8 +3,8 @@ class Road{
         this.x=x;
         this.width=width;
         this.laneCount=laneCount;
-        this.left=x-width/2;
-        this.right=x+width/2;
+        this.left=x-width/4;
+        this.right=x+width/4;
 
         const infinity=1000000;
         this.top=-infinity;
@@ -15,9 +15,16 @@ class Road{
         const topRight={x:this.right,y:this.top};
         const bottomRight={x:this.right,y:this.bottom};
         this.borders=[
-            [topLeft,bottomLeft],
-            [topRight,bottomRight]
-        ]
+            [topLeft],
+            [topRight]
+        ];
+        for(let y=-1000;y<=0;y++){
+            const x=Math.sin(y*0.01)*50;
+            this.borders[0].push({x:x+this.left,y:y});
+            this.borders[1].push({x:x+this.right,y:y});
+        }
+        this.borders[0].push(bottomLeft);
+        this.borders[1].push(bottomRight);
     }
 
     getLaneCenter(laneIndex){
@@ -30,6 +37,7 @@ class Road{
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
+        /*
         ctx.setLineDash([20,20]);
         for(let i=1;i<=this.laneCount-1;i++){
             const x=lerp(
@@ -42,12 +50,15 @@ class Road{
             ctx.lineTo(x,this.bottom);
             ctx.stroke();
         }
+        */
 
         ctx.setLineDash([]);
         this.borders.forEach(border=>{
             ctx.beginPath();
             ctx.moveTo(border[0].x,border[0].y);
-            ctx.lineTo(border[1].x,border[1].y);
+            for(let i=1;i<border.length;i++){
+                ctx.lineTo(border[i].x,border[i].y);
+            }
             ctx.stroke();
         });
     }
