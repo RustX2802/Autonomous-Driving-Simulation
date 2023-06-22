91707f74acbf83ebb08969aa30e84c4ee8cda081
RustX <rustix260685@gmail.com>
2023-06-22 10:42:18 +0900

    sensor

--- car.js
+++ car.js
@@ -11,45 +11,51 @@ class Car{
         this.friction=0.05;
         this.angle=0;
 
+        this.sensor=new Sensor(this);
         this.controls=new Controls();
     }
 
     update(){
+        this.#move();
+        this.sensor.update();
+    }
+
+    #move(){
         if(this.controls.forward){
-           this.speed+=this.acceleration; 
-        }
-        if(this.controls.reverse){
-            this.speed-=this.acceleration;
-        }
-        
-        if(this.speed!=0){
-            const flip=this.speed>0?1:-1;
-            if(this.controls.left){
-                this.angle+=0.03*flip;
-            }
-            if(this.controls.right){
-                this.angle-=0.03*flip;
-            }
-        }
-
-        if(this.speed>this.maxSpeed){
-            this.speed=this.maxSpeed;
-        }
-        if(this.speed<-this.maxSpeed/2){
-            this.speed=-this.maxSpeed/2;
-        }
-
-        if(this.speed>0){
-            this.speed-=this.friction;
-        }
-        if(this.speed<0){
-            this.speed+=this.friction;
-        }
-        if(Math.abs(this.speed)<this.friction){
-            this.speed=0;
-        }
-        this.x-=Math.sin(this.angle)*this.speed;
-        this.y-=Math.cos(this.angle)*this.speed;
+            this.speed+=this.acceleration; 
+         }
+         if(this.controls.reverse){
+             this.speed-=this.acceleration;
+         }
+ 
+         if(this.speed!=0){
+             const flip=this.speed>0?1:-1;
+             if(this.controls.left){
+                 this.angle+=0.03*flip;
+             }
+             if(this.controls.right){
+                 this.angle-=0.03*flip;
+             }
+         }
+ 
+         if(this.speed>this.maxSpeed){
+             this.speed=this.maxSpeed;
+         }
+         if(this.speed<-this.maxSpeed/2){
+             this.speed=-this.maxSpeed/2;
+         }
+ 
+         if(this.speed>0){
+             this.speed-=this.friction;
+         }
+         if(this.speed<0){
+             this.speed+=this.friction;
+         }
+         if(Math.abs(this.speed)<this.friction){
+             this.speed=0;
+         }
+         this.x-=Math.sin(this.angle)*this.speed;
+         this.y-=Math.cos(this.angle)*this.speed;
     }
 
     draw(ctx){
@@ -65,5 +71,6 @@ class Car{
         );
         ctx.fill();
         ctx.restore();
+        this.sensor.draw(ctx);
     }
 }
