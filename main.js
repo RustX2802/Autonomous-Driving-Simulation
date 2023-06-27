e6c557c5ffe7f636e216c80a26f214370faab94a
RustX <rustix260685@gmail.com>
2023-06-27 15:25:29 +0900

    multiple cars

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ networkCanvas.width=298;
 const carCtx=carCanvas.getContext("2d");
 const networkCtx=networkCanvas.getContext("2d");
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
-const car=new Car(100,100,30,50,"AI");
+const N=100;
+const cars=generateCars(N);
 const traffic=[
     new Car(100,-100,30,50,"DUMMY",2)
 ];
@@ -17,20 +18,34 @@ function animate(){
     for(let i=0;i<traffic.length;i++){
         traffic[i].update([],[]);
     }
-
-    car.update(road.borders,traffic);
+    for(let i=0;i<cars.length;i++){
+        cars[i].update(road.borders,traffic);
+    }
 
     carCanvas.height=window.innerHeight;
     networkCanvas.height=window.innerHeight;
     
-    carCtx.translate(0,-car.y+carCanvas.height*0.7);
+    carCtx.translate(0,-cars[0].y+carCanvas.height*0.7);
     road.draw(carCtx);
     for(let i=0;i<traffic.length;i++){
         traffic[i].draw(carCtx);
     }
-    car.draw(carCtx);
+    carCtx.globalAlpha=0.2;
+    for(let i=0;i<cars.length;i++){
+        cars[i].draw(carCtx);
+    }
+    carCtx.globalAlpha=1;
+    cars[0].draw(carCtx,true);
 
-    Visualizer.drawNetwork(networkCtx,car.brain);
+    Visualizer.drawNetwork(networkCtx,cars[0].brain);
     
     requestAnimationFrame(animate);
+}
+
+function generateCars(N){
+    const cars=[];
+    for(let i=1;i<=N;i++){
+        cars.push(new Car(100,100,30,50,"AI"));
+    }
+    return cars;
 }
